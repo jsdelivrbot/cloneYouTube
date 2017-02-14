ba1d5d65dbee1f514580f8215d8d6e96fefcebe9
Clint Ast <clintast@gmail.com>
2017-02-14 15:01:12 -0600

    Added code for search bar in component and added it into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCOk_DQYkfpV77coADf0oI-F1rRMW2UICY';
 // Create a new component.  this component should produce
 // some HTML
@@ -8,7 +10,13 @@ const App = () => {
   //JSX is html like code that you can use in javascript
   // webpack and babel convert the JSX into real javascript
   // it also converts it into html and puts it into the dom
-  return <div>Hi!</div>;
+  return (
+    <div>
+      Hi!
+      <SearchBar />
+    </div>
+  );
+
 }
 
 // Take this component's generated HTML and put it on
