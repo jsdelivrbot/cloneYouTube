5aaadfae3ea07d89580ca983e107f73493b8d818
Clint Ast <clintast@gmail.com>
2017-02-14 15:35:39 -0600

    took out the regular function and added an inline fat arrow function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={(event) => console.log(event.target.value)} />;
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+
 }
 
 export default SearchBar;
@@ -18,5 +16,14 @@ turn this functional component into a class component
 const SearchBar = () => {
   return <input />
 }
+
+cut the following function and added an arrow function
+
+onInputChange(event)  {
+  console.log(event.target.value);
+}
+
+and the calling function
+{this.onInputChange}
 
 */
