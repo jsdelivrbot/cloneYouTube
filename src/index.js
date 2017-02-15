2d6347690541c9105fa1ed76e34988c712d6ceec
Clint Ast <clintast@gmail.com>
2017-02-15 06:26:45 -0600

    coded video list and pasted state to it in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCOk_DQYkfpV77coADf0oI-F1rRMW2UICY';
 
 class App extends Component {
@@ -17,8 +18,8 @@ class App extends Component {
   render() {
     return (
       <div>
-
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
