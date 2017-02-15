7e4cd7f731a1839b619ed38f107d4f5bd8216b35
Clint Ast <clintast@gmail.com>
2017-02-15 06:05:09 -0600

    added search and state to the index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCOk_DQYkfpV77coADf0oI-F1rRMW2UICY';
-// Create a new component.  this component should produce
-// some HTML
-const App = () => {
-  //JSX is html like code that you can use in javascript
-  // webpack and babel convert the JSX into real javascript
-  // it also converts it into html and puts it into the dom
-  return (
-    <div>
-      
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+      <div>
+
+        <SearchBar />
+      </div>
+    );
+  }
 
 }
 
-// Take this component's generated HTML and put it on
-// the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
